82bea57ab5766252480bc84eb6f706c6a6dea681
ARiP001 <142637487+ARiP001@users.noreply.github.com>
2024-10-05 11:44:35 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,18 +1,35 @@
-import streamlit as st
+import os
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-sns.set(style='dark')
+import streamlit as st
 
 
+data_file = r'./dashboard/main_data.csv
+
+@st.cache_data
+def load_data():
+    if os.path.isfile(data_file):
+        df = pd.read_csv(data_file)
+
+        if 'No' in df.columns:
+            df = df.drop(['No'], axis=1)
+          
+        df['datetime'] = pd.to_datetime(df[['year', 'month', 'day', 'hour']])
+        
+        return df
+    else:
+        st.warning(f"File tidak ditemukan di path: {data_file}")
+        return None
+data = load_data()
+
+if data is not None:
+    st.write(data.head())
+else:
+    st.error("Data gagal dimuat. Periksa path file CSV.")
 
 
-st.title("Data Analisis pada Dataset Peminjaman Sepeda")
-
-data_df = pd.read_csv("main_data.csv")
-st.write("Ringkasan Statistik:")
-st.dataframe(data_df.describe())
-
-# Sidebar
+# Sidebar navigasi
 st.sidebar.header("Navigasi")
 st.sidebar.markdown("[Subheader 1](#subheader-1)")
 st.sidebar.markdown("[Subheader 2](#subheader-2)")
