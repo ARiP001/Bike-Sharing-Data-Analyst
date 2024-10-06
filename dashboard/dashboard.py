f579e395255bf752cc076f50e3d025b959038f85
ARiP001 <142637487+ARiP001@users.noreply.github.com>
2024-10-06 18:18:25 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import streamlit as st
 
 # Path ke file CSV lokal
-data_file = 'dashboard/min_data.csv'
+data_file = 'main_data.csv'
 
 # Fungsi untuk memuat dataset
 @st.cache_data
